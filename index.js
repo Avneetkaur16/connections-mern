dfd6eb027c3709c072b07b60a35c41e5981f3e9e
Avneet Kaur <63954648+Avneetkaur16@users.noreply.github.com>
2024-01-04 00:07:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 9000, () => {
     connect();
-    console.log(`Server started on port ${process.env.PORT}`)
+    console.log(`Server started on port ${process.env.PORT || 9000}`)
 })
